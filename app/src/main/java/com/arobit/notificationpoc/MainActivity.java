f6c88ef4ed736b409120bdaa77a4706e3d34d5db
soumoq <soumojyotiganguly270@gmail.com>
2021-05-21 12:41:30 +0530

    Create Broadcast receiver and notification with toast button

--- app/src/main/java/com/arobit/notificationpoc/MainActivity.java
+++ app/src/main/java/com/arobit/notificationpoc/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,12 +34,25 @@ public class MainActivity extends AppCompatActivity {
         channel1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent activityIntent = new Intent(v.getContext(),MainActivity.class);
+                PendingIntent contentIntent = PendingIntent.getActivities(v.getContext(),0, new Intent[]{activityIntent},0);
+
+                Intent broadcastIntent = new Intent(v.getContext(),NotificationReceiver.class);
+                broadcastIntent.putExtra("toast","Hi I'm channel one");
+                PendingIntent actionIntent = PendingIntent.getBroadcast(v.getContext(),0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
                 Notification notification = new NotificationCompat.Builder(v.getContext(),App.CHANNEL_1_ID)
                         .setSmallIcon(R.drawable.ic_baseline_exposure_plus_1_24)
                         .setContentTitle("Channel_1")
                         .setContentText("This is channel 1")
                         .setPriority(Notification.PRIORITY_HIGH)
                         .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .setColor(Color.BLUE)
+                        .setContentIntent(contentIntent)
+                        .setAutoCancel(true)
+                        .setOnlyAlertOnce(true)
+                        .addAction(R.mipmap.ic_launcher,"Toast",actionIntent)
                         .build();
                 notificationManager.notify(1,notification);
             }
